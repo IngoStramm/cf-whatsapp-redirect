155841908c2447c95319409abcb814720b9d1627
Ingo Stramm <ingostramm@gmail.com>
2021-07-01 16:36:41 -0300

    versão 1.2 novos recursos

--- cwr-functions.php
+++ cwr-functions.php
@@ -23,9 +23,41 @@ function cwr_get_option($key = '', $default = false)
 
 add_action('wp_footer', function () {
 ?>
+    <?php
+    $cwr_enable_dynamic_event_name = cwr_get_option('cwr_enable_dynamic_event_name');
+    $cwr_event_name = 'whatsapp';
+    if ($cwr_enable_dynamic_event_name === 'on') {
+        global $post;
+        $cwr_event_name .=  '.' . $post->post_name;
+    }
+    // cwr_debug($cwr_event_name);
+    ?>
     <script>
+        cwr_trigger_element_evt = (cwr_wpp_element) => {
+            cwr_wpp_element.addEventListener('click', (evt) => {
+                // evt.preventDefault();
+                // console.log('click');
+                console.log('<?php echo $cwr_event_name; ?>');
+                if (typeof(dataLayer) != 'undefined' && dataLayer != null) {
+                    dataLayer.push({
+                        'event': '<?php echo $cwr_event_name; ?>'
+                    });
+                }
+            });
+        };
+    </script>
+    <?php
+    $cwr_disable_rd_station_tracking = cwr_get_option('cwr_disable_rd_station_tracking');
+    // cwr_debug($cwr_disable_rd_station_tracking);
+    ?>
+    <script>
+        console.log('footer');
         // Caso o botão do whatsapp tenha sido adicionado pelo RD Station
-        window.onload = () => {
+        const cwr_rd_wpp = () => {
+            <?php if ($cwr_disable_rd_station_tracking === 'on') { ?>
+                return;
+            <?php } ?>
+            console.log('load');
             let count = 1;
             let cwr_verifica_btn = setInterval(() => {
 
@@ -43,7 +75,7 @@ add_action('wp_footer', function () {
 
                             let validate_fields = true;
                             let cwr_fields_data_object = {};
-                            cwr_fields_data_object.event = 'whatsapp';
+                            cwr_fields_data_object.event = '<?php echo $cwr_event_name; ?>';
                             Array.prototype.forEach.call(cwr_fields, (cwr_field, i) => {
                                 if (cwr_field.value.length === 0 || cwr_field.parentNode.classList.contains('has-danger')) {
                                     validate_fields = false;
@@ -78,75 +110,116 @@ add_action('wp_footer', function () {
 
             const cwr_others_wpp_btns = document.getElementsByClassName('trigger-gtm-wpp');
             Array.prototype.forEach.call(cwr_others_wpp_btns, (cwr_others_wpp_btn) => {
-                cwr_others_wpp_btn.addEventListener('click', () => {
-                    if (typeof(dataLayer) != 'undefined' && dataLayer != null) {
-                        dataLayer.push({
-                            'event': 'whatsapp'
-                        });
-                    }
-                });
+                cwr_trigger_element_evt(cwr_others_wpp_btn);
             });
         };
     </script>
     <script>
         // Caso haja botões inseridos através do Elementor (ou de outra forma)
         // basta adicionar a classe "trigger-gtm-wpp" no botão/link
-        window.onload = () => {
+        const cwr_trigger_gtm_wpp = () => {
+            console.log('cwr_trigger_gtm_wpp');
             const cwr_others_wpp_btns = document.getElementsByClassName('trigger-gtm-wpp');
             Array.prototype.forEach.call(cwr_others_wpp_btns, (cwr_others_wpp_btn) => {
-                cwr_others_wpp_btn.addEventListener('click', () => {
-                    if (typeof(dataLayer) != 'undefined' && dataLayer != null) {
-                        dataLayer.push({
-                            'event': 'whatsapp'
-                        });
-                    }
-                });
+                cwr_trigger_element_evt(cwr_others_wpp_btn);
             });
         };
     </script>
     <script>
         // Caso o botão do Whatsapp tenha sido adicionado pelo plugin Click to Chat (padrão nos CF)
-        window.onload = () => {
+        cwr_click_to_chat = () => {
+            <?php
+            $cwr_get_active_plugins = get_option('active_plugins');
+            if (!in_array('click-to-chat-for-whatsapp/click-to-chat.php', $cwr_get_active_plugins)) { ?>
+                return;
+            <?php } ?>
+            console.log('cwr_click_to_chat');
             const ctc_wpp_divs = document.getElementsByClassName('ht-ctc-chat');
             Array.prototype.forEach.call(ctc_wpp_divs, (ctc_wpp_div) => {
-                ctc_wpp_div.addEventListener('click', (evt) => {
-                    if (typeof(dataLayer) != 'undefined' && dataLayer != null) {
-                        dataLayer.push({
-                            'event': 'whatsapp'
-                        });
-                    }
-                });
+                cwr_trigger_element_evt(ctc_wpp_div);
             });
         };
     </script>
+    <script>
+        cwr_css_classes_evt = (cwr_css_classes_array, parent = false) => {
+            if (cwr_css_classes_array.length > 0) {
+                for (let index = 0; index < cwr_css_classes_array.length; index++) {
+                    const cwr_wpp_elements = document.getElementsByClassName(cwr_css_classes_array[index]);
+                    Array.prototype.forEach.call(cwr_wpp_elements, (cwr_wpp_element) => {
+                        let cwr_trigger_element;
+                        if (!parent) {
+                            cwr_trigger_element_evt(cwr_wpp_element);
+                        } else {
+                            const cwr_wpp_elements_links = cwr_wpp_element.getElementsByTagName('a');
+                            Array.prototype.forEach.call(cwr_wpp_elements_links, (cwr_wpp_elements_link) => {
+                                cwr_trigger_element_evt(cwr_wpp_elements_link);
+                            });
+                        }
+                    });
+                }
+            }
+        };
+    </script>
     <?php
-    $cwr_css_classes = cwr_get_option('cwr_css_classes');
-    if (!empty($cwr_css_classes) && !is_null($cwr_css_classes)) {
+    $cwr_direct_css_classes = cwr_get_option('cwr_direct_css_classes');
+    if (!empty($cwr_direct_css_classes) && !is_null($cwr_direct_css_classes)) {
     ?>
         <script>
-            window.onload = () => {
-                const cwr_css_classes_array = [];
-                <?php foreach ($cwr_css_classes as $cwr_css_class) { ?>
-                    cwr_css_classes_array.push('<?php echo $cwr_css_class; ?>');
+            cwr_direct_link_classes = () => {
+                const cwr_direct_css_classes_array = [];
+                <?php foreach ($cwr_direct_css_classes as $cwr_css_class) { ?>
+                    cwr_direct_css_classes_array.push('<?php echo $cwr_css_class; ?>');
                 <?php } ?>
-                if (cwr_css_classes_array.length > 0) {
-                    for (let index = 0; index < cwr_css_classes_array.length; index++) {
-                        const cwr_wpp_elements = document.getElementsByClassName(cwr_css_classes_array[index]);
-                        Array.prototype.forEach.call(cwr_wpp_elements, (cwr_wpp_element) => {
-                            cwr_wpp_element.addEventListener('click', (evt) => {
-                                if (typeof(dataLayer) != 'undefined' && dataLayer != null) {
-                                    dataLayer.push({
-                                        'event': 'whatsapp'
-                                    });
-                                }
-                            });
-                        });
-                    }
-                }
+                cwr_css_classes_evt(cwr_direct_css_classes_array);
             };
         </script>
     <?php
     }
     ?>
+    <script>
+        cwr_parent_link_classes = () => {
+            <?php
+            $cwr_parent_css_classes = cwr_get_option('cwr_parent_css_classes');
+            if (empty($cwr_parent_css_classes) || is_null($cwr_parent_css_classes)) { ?>
+                return;
+            <?php } else { ?>
+                // console.log('cwr_parent_link_classes');
+                const cwr_parent_css_classes_array = [];
+                <?php foreach ($cwr_parent_css_classes as $cwr_css_class) { ?>
+                    cwr_parent_css_classes_array.push('<?php echo $cwr_css_class; ?>');
+                <?php } ?>
+                cwr_css_classes_evt(cwr_parent_css_classes_array, true);
+            <?php } ?>
+        };
+    </script>
+    <?php
+    $cwr_enable_all_links = cwr_get_option('cwr_enable_all_links');
+    // cwr_debug($cwr_enable_all_links);
+    ?>
+    <script>
+        const cwr_all_links_evt = () => {
+            <?php if ($cwr_enable_all_links !== 'on') { ?>
+                return;
+            <?php } ?>
+            console.log('cwr_enable_all_links');
+            const cwr_all_links = document.getElementsByTagName('a');
+            Array.prototype.forEach.call(cwr_all_links, (cwr_link) => {
+                if (cwr_link.href.includes('whatsapp')) {
+                    console.log(cwr_link.href);
+                    cwr_trigger_element_evt(cwr_link);
+                }
+            });
+        };
+    </script>
+    <script>
+        window.onload = () => {
+            cwr_rd_wpp();
+            cwr_trigger_gtm_wpp();
+            cwr_click_to_chat();
+            cwr_direct_link_classes();
+            cwr_parent_link_classes();
+            cwr_all_links_evt();
+        };
+    </script>
 <?php
 }, 9999);
--- cwr-options.php
+++ cwr-options.php
@@ -17,7 +17,7 @@ add_action('cmb2_admin_init', function () {
 
     $cmb->add_field(array(
         'name'    => esc_html__('Observações', 'cf-whatsapp-redirect'),
-        'id'      => 'cwr_instructions',
+        'id'      => 'cwr_instructions_1',
         'type' => 'title',
         'after' => function() {
             $output = '';
@@ -33,10 +33,54 @@ add_action('cmb2_admin_init', function () {
     ));
 
     $cmb->add_field(array(
-        'name'    => esc_html__('Classe CSS dos elementos que abrem a janela de chat do Whastapp', 'cf-whatsapp-redirect'),
+        'name'    => esc_html__('Classe CSS direta dos elementos que abrem a janela de chat do Whastapp', 'cf-whatsapp-redirect'),
         'desc'    => esc_html__('Os elementos podem ser um botão, um link, um ícone ou qualquer elemento que seja usado para redirecionar o usuário para a janela de chat do Whastapp.', 'cf-whatsapp-redirect'),
         'id'      => 'cwr_css_classes',
         'type'    => 'text',
         'repeatable' => true,
     ));
+
+    $cmb->add_field(array(
+        'name'    => esc_html__('Classe CSS pai dos elementos que abrem a janela de chat do Whastapp', 'cf-whatsapp-redirect'),
+        'desc'    => esc_html__('Os elementos podem ser uma div, section ou qualquer outro elemento que possua um botão, link, ícone, etc, que seja usado para redirecionar o usuário para a janela de chat do Whastapp.', 'cf-whatsapp-redirect'),
+        'id'      => 'cwr_parent_css_classes',
+        'type'    => 'text',
+        'repeatable' => true,
+    ));
+
+    $cmb->add_field(array(
+        'name'    => esc_html__('Opções Extras', 'cf-whatsapp-redirect'),
+        'id'      => 'cwr_instructions_2',
+        'type' => 'title',
+        'after' => function () {
+            $output = '';
+            $output .= '&nbsp;';
+            $output .= '<p>' . esc_html__('Nas opções abaixo, é possível habilitar ou desativar alguns recursos do plugin.', 'cf-whatsapp-redirect') . '</p>';
+            $output .= '&nbsp;';
+            return $output;
+        }
+    ));
+
+    $cmb->add_field(array(
+        'name' => esc_html__('Desabilitar o rastreio do botão do RD-Station', 'cmb2'),
+        'desc' => esc_html__('Por padrão, este plugin procura o botão do whatsapp inserido pelo RD-Station e faz o rastreio dele. Se o seu site não utiliza o botão do Whatsapp gerado pelo RD-Station, deixe esta opção MARCADA para desativar este recurso.', 'cmb2'),
+        'id'   => 'cwr_disable_rd_station_tracking',
+        'type' => 'checkbox',
+    ));
+
+    $cmb->add_field(array(
+        'name' => esc_html__('Habilitar a opção de procurar todos os links do site que possuam a palavra "whatsapp" no atributo "href"', 'cmb2'),
+        'desc' => esc_html__('Com esta opção ativada, não é necessário utilizar uma classe CSS para buscar os links do botão do Whatsapp. Obs: não funciona com o botão gerado pelo RD Station.', 'cmb2'),
+        'id'   => 'cwr_enable_all_links',
+        'type' => 'checkbox',
+    ));
+
+    $cmb->add_field(array(
+        'name' => esc_html__('Habilitar a opção de definir o nome do evento dinamicamente a partir da url da página', 'cmb2'),
+        'desc' => esc_html__('Com esta opção ativada, o nome do evento de rastreio terá o sufixo com o slug da página de onde o evento se originou. Por exemplo, na página "https://seusite.com.br/contato", o nome do evento de rastreio ficaria "whatsapp.contato".', 'cmb2'),
+        'id'   => 'cwr_enable_dynamic_event_name',
+        'type' => 'checkbox',
+    ));
+
+
 });
